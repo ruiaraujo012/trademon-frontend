2ee54421993a956044fe3c0bc0e6bbd2ba199173
Rui Araújo <ruiaraujo012@gmail.com>
2020-04-11 11:20:08 +0100

    Updates

---
+++ precache-manifest.0f793269767a5309d754595d0abeab6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cb23c320fee54d320760ecbcad94652",
+    "url": "/PoGoTrades-Backend/index.html"
+  },
+  {
+    "revision": "0eeec46daaa2bd80b8d0",
+    "url": "/PoGoTrades-Backend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "af3e45edb09fdb6b323e",
+    "url": "/PoGoTrades-Backend/static/js/2.935eb933.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/PoGoTrades-Backend/static/js/2.935eb933.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0eeec46daaa2bd80b8d0",
+    "url": "/PoGoTrades-Backend/static/js/main.a4e839c7.chunk.js"
+  },
+  {
+    "revision": "7b94ea74f48c4b34aa4a",
+    "url": "/PoGoTrades-Backend/static/js/runtime-main.9fbccc40.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/PoGoTrades-Backend/static/media/logo.5d5d9eef.svg"
+  }
+]);
