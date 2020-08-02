308455c21c2e8388a0e6d3a3503a599fd361bffd
Rui Araújo <ruiaraujo012@gmail.com>
2020-08-03 00:39:01 +0100

    :sparkles: Unauthorized page added
    Not found page refactored.

--- src/components/notFoundPage/NotFoundPage.jsx
+++ src/components/notFoundPage/NotFoundPage.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Button } from "@material-ui/core";
-import PageNotFound from "../../images/page404.png";
+import NotFoundImage from "../../images/404image.jpg";
 import "./NotFoundPage.css";
 
 class NotFoundPage extends React.Component {
   render() {
     return (
       <div>
-        <img src={PageNotFound} className="center-image" alt="Not found" />
+        <img
+          src={NotFoundImage}
+          className="center-image"
+          alt="Not found"
+          width="100%"
+          style={{ borderRadius: "20px" }}
+        />
+
         <p style={{ textAlign: "center" }}>
           <Button variant="outlined" color="primary">
             <Link to="/" className="custom-link">
