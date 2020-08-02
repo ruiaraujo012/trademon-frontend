65b62bf2fe448330f8c7b24e9712f0430b0d1e82
Rui Araújo <ruiaraujo012@gmail.com>
2020-08-02 21:15:07 +0100

    :construction: Change api uri

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const url = "http://localhost:8000/v1/";
+const url = "http://localhost:8000/v1/";
 // const url = "http://192.168.1.178:8000/v1/";
-const url = "http://192.168.1.96:8000/v1/";
+// const url = "http://192.168.1.96:8000/v1/";
 
 const instance = axios.create({
   baseURL: url,
