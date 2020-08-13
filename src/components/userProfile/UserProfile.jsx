addbe2c71d4984276113340ffb4e20e25e6dbfed
Rui Araújo <ruiaraujo012@gmail.com>
2020-08-13 20:48:38 +0100

    🚧 Edit user data dialog begin

--- src/components/userProfile/UserProfile.jsx
+++ src/components/userProfile/UserProfile.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import API from "../../utils/api";
 import {
   Card,
@@ -10,6 +10,12 @@ import {
   Tooltip,
   Typography,
   Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Zoom,
 } from "@material-ui/core";
 import { Skeleton } from "@material-ui/lab";
 
@@ -19,126 +25,153 @@ import Blue from "../../images/blue.jpg";
 import Red from "../../images/red.jpg";
 import Unown from "../../images/unown.jpg";
 
-export class UserProfile extends Component {
-  constructor(props) {
-    super(props);
+const UserProfile = () => {
+  const [loading, setLoading] = useState(true);
+  const [image, setImage] = useState(Unown);
+  const [user, setUser] = useState(null);
+  const [openEditDialog, setOpenEditDialog] = useState(false);
 
-    this.state = {
-      loading: true,
-      image: Unown,
-      userData: null,
-    };
-  }
+  useEffect(() => {
+    getUserData();
+  }, []); // Equals to componenteDidMount
 
-  async componentDidMount() {
+  /**
+   * Functions
+   */
+
+  const getUserData = async () => {
     try {
       const { data } = await API.get("/users/profile");
-      this.setState({ userData: data });
+      setUser(data);
       console.log("data :>> ", data);
 
-      this.setTeamImage(data.teamName);
+      setTeamImage(data.teamName);
 
-      this.setState({ loading: false });
+      setLoading(false);
     } catch (err) {
       console.log("err :>> ", err);
     }
-  }
+  };
 
-  setTeamImage = (teamName) => {
+  const setTeamImage = (teamName) => {
     switch (teamName) {
       case "Instinct":
-        this.setState({ image: Yellow });
+        setImage(Yellow);
         break;
       case "Mystic":
-        this.setState({ image: Blue });
+        setImage(Blue);
         break;
       case "Valor":
-        this.setState({ image: Red });
+        setImage(Red);
         break;
 
       default:
-        this.setState({ image: Unown });
+        setImage(Unown);
         break;
     }
   };
 
-  render() {
-    let loading = this.state.loading;
+  const handleClickEdit = () => {
+    setOpenEditDialog(true);
+  };
 
-    const SkeletonContent = (
-      <Box display="flex" alignItems="center">
-        <Box width="100%">
-          <Typography variant="h1">
-            <Skeleton animation="wave" width="50%" />
-          </Typography>
-        </Box>
-        <Box width="auto">
-          <Skeleton animation="wave" variant="circle" width={40} height={40} />
-        </Box>
+  const handleClose = () => {
+    setOpenEditDialog(false);
+  };
+
+  /**
+   * Small components
+   */
+
+  const SkeletonContent = (
+    <Box display="flex" alignItems="center">
+      <Box width="100%">
+        <Typography variant="h1">
+          <Skeleton animation="wave" width="50%" />
+        </Typography>
       </Box>
-    );
+      <Box width="auto">
+        <Skeleton animation="wave" variant="circle" width={40} height={40} />
+      </Box>
+    </Box>
+  );
 
-    return (
-      <div>
-        <Card>
-          <CardContent>
-            {!loading ? (
-              <CardMedia
-                component="img"
-                alt="Team"
-                height="300"
-                image={this.state.image}
-              />
-            ) : (
-              <Skeleton animation="wave" variant="rect" height={300} />
-            )}
+  const EditDialog = (
+    <Dialog
+      open={openEditDialog}
+      TransitionComponent={Zoom}
+      // onClose={handleClose}
+    >
+      <DialogTitle id="form-dialog-title">Edit Profile</DialogTitle>
+      <DialogContent>
+        Test
+        {/* <TextField
+          autoFocus
+          margin="dense"
+          id="name"
+          label="Email Address"
+          type="email"
+          fullWidth
+        /> */}
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={handleClose} color="secondary">
+          Cancel
+        </Button>
+        {/* <Button onClick={handleSave} color="primary"> */}
+        <Button color="primary">Save</Button>
+      </DialogActions>
+    </Dialog>
+  );
 
-            {!loading ? (
-              <Grid
-                container
-                direction="row"
-                alignItems="center"
-                justify="space-between"
-                spacing={3}
-              >
-                <Grid item xl="auto">
-                  <h1>
-                    <Badge
-                      anchorOrigin={{
-                        vertical: "top",
-                        horizontal: "right",
-                      }}
-                      badgeContent={`Level: ${this.state.userData.level}`}
-                      color="primary"
-                    >
-                      {this.state.userData.username}
-                    </Badge>
-                  </h1>
-                </Grid>
-                <Grid item xl="auto">
-                  <Tooltip title="Edit profile">
-                    <IconButton
-                      onClick={() => {
-                        alert("Edit Profile");
-                      }}
-                    >
-                      <EditTwoTone />
-                    </IconButton>
-                  </Tooltip>
-                </Grid>
+  return (
+    <div>
+      <>{EditDialog}</>
+      <Card>
+        <CardContent>
+          {!loading ? (
+            <CardMedia component="img" alt="Team" height="300" image={image} />
+          ) : (
+            <Skeleton animation="wave" variant="rect" height={300} />
+          )}
+
+          {!loading ? (
+            <Grid
+              container
+              direction="row"
+              alignItems="center"
+              justify="space-between"
+              spacing={3}
+            >
+              <Grid item xl="auto">
+                <h1>
+                  <Badge
+                    anchorOrigin={{
+                      vertical: "top",
+                      horizontal: "right",
+                    }}
+                    badgeContent={`Level: ${user.level}`}
+                    color="primary"
+                  >
+                    {user.username}
+                  </Badge>
+                </h1>
               </Grid>
-            ) : (
-              <>{SkeletonContent}</>
-            )}
-          </CardContent>
-        </Card>
-        {/* {this.state.userData &&
-          Object.keys(this.state.userData).map((d) => {
-            return <p>{this.state.userData[d]}</p>;
-          })} */}
-      </div>
-    );
-  }
-}
+              <Grid item xl="auto">
+                <Tooltip title="Edit profile">
+                  <IconButton onClick={handleClickEdit}>
+                    <EditTwoTone />
+                  </IconButton>
+                </Tooltip>
+              </Grid>
+            </Grid>
+          ) : (
+            <>{SkeletonContent}</>
+          )}
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
 
 export default UserProfile;
